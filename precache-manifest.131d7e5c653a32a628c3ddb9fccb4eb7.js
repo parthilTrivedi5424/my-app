5f778d35f2946747c3ff445bbbf5a8984692a7b5
Parthil Trivedi <parthilgit@gmail.com>
2022-08-18 18:30:38 +0530

    Updates

---
+++ precache-manifest.131d7e5c653a32a628c3ddb9fccb4eb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "646342dd5ba3fd4275ae",
+    "url": "/my-app/static/css/main.51536242.chunk.css"
+  },
+  {
+    "revision": "646342dd5ba3fd4275ae",
+    "url": "/my-app/static/js/main.c76a3d4f.chunk.js"
+  },
+  {
+    "revision": "88ae9c2a0c24c803033a",
+    "url": "/my-app/static/js/runtime~main.cb9d6767.js"
+  },
+  {
+    "revision": "206cc79f746b27c99aa2",
+    "url": "/my-app/static/js/2.586aa630.chunk.js"
+  },
+  {
+    "revision": "c0c98a6c240aa4e0d888",
+    "url": "/my-app/static/js/3.68b36b5f.chunk.js"
+  },
+  {
+    "revision": "84eaf77cdfd3aabd1342f4a7b3bdeabd",
+    "url": "/my-app/index.html"
+  }
+];
